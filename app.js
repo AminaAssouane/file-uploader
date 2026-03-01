4632149e09b89f7a65b393e55a376bc25d5d4e9f
Amina <minassouane@gmail.com>
2026-03-01 23:26:54 +0100

    feat: set up MVC structure with routes, controllers, and views

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+const fileRouter = require("./routes/fileRouter");
+
+app.set("view", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", fileRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
---
+++ routes/fileRouter.js
@@ -0,0 +1,4 @@
+const { Router } = require("express");
+const fileRouter = Router();
+
+module.exports = fileRouter;
