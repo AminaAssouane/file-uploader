d2f78d8bd0edea1a0f694752ed461aba0d5fcdf3
Amina <minassouane@gmail.com>
2026-03-03 11:50:20 +0100

    feat(session): configure Express to persist sessions in database with Prisma

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ const fileRouter = require("./routes/fileRouter");
 const session = require("express-session");
 const passport = require("passport");
 require("./config/passport");
+const prisma = require("./db/prismaClient");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 
 const app = express();
 
 app.set("view", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "cats",
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      modelName: "Session",
+    }),
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 app.use(passport.session());
 
 app.use("/", fileRouter);
