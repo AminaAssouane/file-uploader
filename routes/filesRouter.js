b695e833cbee25e79b7a3d16e6421ad626553645
Amina <minassouane@gmail.com>
2026-03-04 15:55:42 +0100

    feat(files): add files routes

---
+++ routes/filesRouter.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const filesController = require("../controllers/filesController");
+const filesRouter = Router();
+const ensureAuthenticated = require("../middleware/authMiddleware");
+
+filesRouter.get("/files", ensureAuthenticated, filesController.listFilesByUser);
+filesRouter.get(
+  "/folders/:id/files",
+  ensureAuthenticated,
+  filesController.listFilesByFolder,
+);
+filesRouter.get("/files/:id", ensureAuthenticated, filesController.getFileById);
+
+module.exports = filesRouter;
